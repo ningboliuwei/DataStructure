279ddac8e83a916b42edab0f70f309810bf0ea44
ningboliuwei <liuwei@nbu.edu.cn>
2018-09-05 22:58:37 +0800

    same?

--- Chapter2/SingleLinkList/SingleLinkList_1.c
+++ Chapter2/SingleLinkList/SingleLinkList_1.c
@@ -90,34 +90,81 @@ void ShowList_L(LinkList linklist) {
     }
 }
 
-Node *SearchListByIndex_L(LinkList linkList, int index) {
+// 根据位置搜索链表中的元素
+Node *SearchListByPos_L(LinkList linkList, int pos) {
     Node *p = linkList;
     int i = 0;
 
-    while (p->next != NULL && i < index) {
+    while (p->next != NULL && i < pos) {
         p = p->next;
         i++;
     }
 
-    if (i == index) {
+    if (i == pos) {
         return p;
     } else {
         return NULL;
     }
 }
 
-Node *SearchListByElement_L(LinkList linkList, int element) {
+// 根据某元素搜索在链表中的位置
+int SearchListByElement_L(LinkList linkList, int element) {
     Node *p = linkList->next;
+    int pos = 1;
 
     while (p != NULL && p->data != element) {
         p = p->next;
+        pos++;
     }
 
     if (p->data == element) {
-        return p;
+        return pos;
     } else {
-        return NULL;
+        return -1;
     }
+}
+
+// 删除链表中指定位置的元素
+int DelList_L(LinkList linkList, int pos) {
+    Node *p = linkList;
+    int index = 0;
+
+    while (p->next != NULL && index < pos - 1) {
+        p = p->next;
+        index++;
+    }
+
+    if (p->next == NULL || index > pos - 1) {
+        return -1;
+    } else {
+        Node *q = p->next;
+        p->next = q->next;
+        free(q);
+
+        return 1;
+    }
+}
+
+// 我自己的写法
+int DelList_L2(LinkList linkList, int pos) {
+    Node *p = linkList;
+    int index = 0;
+
+    if (pos <= 0 || pos > LengthList_L(linkList)) {
+        return -1;
+    }
+
+    // 这里的 p->next == NULL 判定可以精简吗？
+    while (index < pos - 1) {
+        p = p->next;
+        index++;
+    }
+
+    Node *q = p->next;
+    p->next = q->next;
+    free(q);
+
+    return 1;
 }
 
 
@@ -134,23 +181,36 @@ int main() {
     printf("The length of the linklist is %d.", length);
     printf("\n");
 
-    printf("\n");
-    printf("Input a index you want to search.");
-    printf("\n");
+//    printf("\n");
+//    printf("Input a index you want to search.");
+//    printf("\n");
+//
+//    int index = 0;
+//    scanf("%d", &index);
+//    Node *result = SearchListByPos_L(linkList, index);
+//    printf("The found element is %d", result->data);
+//
+//    printf("\n");
+//    printf("Input a element you want to search.");
+//    printf("\n");
+//
+//    int element = 0;
+//    scanf("%d", &element);
+//    int pos = SearchListByElement_L(linkList, element);
+//
+//    printf("\n");
+//    printf("The found position is %d", pos);
 
-    int index = 0;
-    scanf("%d", &index);
-    Node *result = SearchListByIndex_L(linkList, index);
-    printf("The found element is %d", result->data);
-
+    int pos;
     printf("\n");
-    printf("Input a element you want to search.");
-    printf("\n");
+    printf("Input the position of element you want to delete");
+    scanf("%d", &pos);
+    int q = DelList_L2(linkList, pos);
 
-    int element = 0;
-    scanf("%d", &element);
-    Node *address = SearchListByElement_L(linkList, element);
-    printf("The found address is %x", address);
+    printf("%d\n", q);
+    printf("The elements of the linklist after deletion:");
+    ShowList_L(linkList);
+    printf("\n");
 
     getchar();
 }
