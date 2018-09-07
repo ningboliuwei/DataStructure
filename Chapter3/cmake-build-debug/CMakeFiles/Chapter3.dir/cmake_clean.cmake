1747e5a095f94118a2df6a0000115684a20ab49d
ningboliuwei <liuwei@nbu.edu.cn>
2018-09-07 15:35:20 +0800

    修改 ignore，去掉所有 cmake-buid-debug 文件夹下的东西

--- Chapter3/cmake-build-debug/CMakeFiles/Chapter3.dir/cmake_clean.cmake
+++ Chapter3/cmake-build-debug/CMakeFiles/Chapter3.dir/cmake_clean.cmake
@@ -1,11 +1,5 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/Chapter3.dir/main.c.o"
-  "Chapter3.pdb"
-  "Chapter3.exe"
-  "libChapter3.dll.a"
-)
 
 # Per-language clean rules from dependency scanning.
-foreach(lang C)
+foreach(lang )
   include(CMakeFiles/Chapter3.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
